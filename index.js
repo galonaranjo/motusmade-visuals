0b045dcd94e58102e9101ef80b5495f4cbc02fe6
Galo Naranjo <gnaranjo411@gmail.com>
2024-10-31 14:22:31 -0400

    more images

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require("express");
 const app = express();
 
 let visuals = [
-  { id: "1", url: "/images/1.jpg" },
-  { id: "2", url: "/images/2.jpg" },
-  { id: "3", url: "/images/3.jpg" },
-  { id: "4", url: "/images/4.jpg" },
-  { id: "5", url: "/images/5.jpg" },
+  { id: "1", url: "/images/001.jpg" },
+  { id: "2", url: "/images/002.jpg" },
+  { id: "3", url: "/images/003.jpg" },
+  { id: "4", url: "/images/004.jpg" },
+  { id: "5", url: "/images/005.jpg" },
+  { id: "6", url: "/images/006.jpg" },
+  { id: "7", url: "/images/007.jpg" },
+  { id: "8", url: "/images/008.jpg" },
+  { id: "9", url: "/images/009.jpg" },
+  { id: "10", url: "/images/010.jpg" },
+  { id: "11", url: "/images/011.jpg" },
+  { id: "12", url: "/images/012.jpg" },
 ];
 
 app.set("view engine", "ejs");
